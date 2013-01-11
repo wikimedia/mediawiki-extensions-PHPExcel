c195982345616c6d04c9d3308ed57d6db77d992a
Kim Eik <kim@heldig.org>
2013-01-11 08:55:27 +0100

    Initial commit of PHPExcel v. 1.7.8 (stable)

---
+++ PHPExcel.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!defined('MEDIAWIKI')) {
+	die('Not an entry point.');
+}
+
+if (!extension_loaded('zip')) {
+	die('Missing zip extension.');
+}
+
+if (!extension_loaded('xml')) {
+	die('Missing xml extension.');
+}
+
+if (!extension_loaded('iconv')) {
+	die('Missing iconv extension.');
+}
+
+if (!extension_loaded('gd2')) {
+	//trigger_error('Missing gd2 extension.');
+}
+
+$wgExtensionCredits['other'][] = array(
+       'path' => __FILE__,
+       'name' => 'PHPExcel',
+       'author' =>'Kim Eik', 
+       'url' => 'https://www.mediawiki.org/wiki/Extension:PHPExcel', 
+       'description' => 'This extension is a container for the PHPExcel library',
+       'version'  => '1.7.8',
+);
+
+## PHPExcel library
+$wgAutoloadClasses['PHPExcel'] = dirname(__FILE__) . '/includes/PHPExcel.php';
